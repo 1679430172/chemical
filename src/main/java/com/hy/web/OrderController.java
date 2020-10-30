b4b4aaefcc633dfb8fd7f4e7e59ad3039fc3cea6
lss <1679430172@qq.com>
2020-10-30 13:53:49 +0800

    架构完善2

--- src/main/java/com/hy/web/OrderController.java
+++ src/main/java/com/hy/web/OrderController.java
@@ -1,4 +1,26 @@
 package com.hy.web;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hy.bean.Order;
+import com.hy.service.OrderService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Api
+@Controller
+@RequestMapping("Order")
 public class OrderController {
+    @Autowired
+    private OrderService service;
+
+    @RequestMapping("/select")
+    @ResponseBody
+    public List<Order> select() throws Exception {
+        return service.list();
+    }
 }
