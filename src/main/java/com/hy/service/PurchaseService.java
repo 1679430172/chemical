3db8b32f892ac22fc2e1ba3fa1556796b2211eac
xiaojian <1124421223@qq.com>
2020-11-04 16:57:35 +0800

    11.4库存,采购

---
+++ src/main/java/com/hy/service/PurchaseService.java
@@ -0,0 +1,21 @@
+package com.hy.service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hy.bean.Purchase;
+import com.hy.mapper.PurchaseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PurchaseService extends ServiceImpl<PurchaseMapper, Purchase> {
+    @Autowired
+    private PurchaseMapper purchaseMapper;
+
+    public IPage<Purchase> iPage(Integer page, Integer limit){
+        IPage<Purchase> iPage=purchaseMapper.Purchase(new Page(page,limit));
+        return iPage;
+    }
+
+}
--- src/main/java/com/hy/web/InventoryControlledr.java
+++ src/main/java/com/hy/web/InventoryControlledr.java
@@ -3,11 +3,13 @@ package com.hy.web;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.hy.bean.Inventory;
+import com.hy.bean.Order;
 import com.hy.service.InventoService;
 import com.hy.util.ParseData;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,6 +25,13 @@ public class InventoryControlledr {
     public ParseData inventory(Integer page, Integer limit){
         IPage<Inventory> iPage= InventoService.iPage(page,limit);
         return new ParseData(0,"",Integer.parseInt(Long.toString(iPage.getTotal())),iPage.getRecords());
+    }
+
+    @PostMapping("/addinventory.do")
+    @ResponseBody
+    public Integer add(Inventory inventory) throws Exception {
+        System.out.println(inventory);
+        return 1;
     }
 
 }
