aeb5531dd8183c8a6829950f66ecaf58336c9e6b
zhangzhemeizi <1290812064@qq.com>
2020-11-04 08:51:06 +0800

    11.4

--- src/main/java/com/hy/mapper/InvoiceMapper.java
+++ src/main/java/com/hy/mapper/InvoiceMapper.java
@@ -2,7 +2,14 @@ package com.hy.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hy.bean.Invoice;
-import com.hy.bean.Order;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+@Mapper
 public interface InvoiceMapper extends BaseMapper<Invoice> {
+    @Select("select * from Invoice where cas=#{cas}")
+    public List<Invoice> queryBycas(String cas);
+
+
 }
--- src/main/java/com/hy/web/InvoiceController.java
+++ src/main/java/com/hy/web/InvoiceController.java
@@ -1,16 +1,18 @@
 package com.hy.web;
 
 import com.hy.bean.Invoice;
-import com.hy.bean.Order;
 import com.hy.service.InvoiceService;
-import com.hy.service.OrderService;
 import com.hy.util.ParseData;
+import com.hy.util.Util;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Api
@@ -32,4 +34,28 @@ public class InvoiceController  {
         parseData.setData(list);
         return parseData;
     }
+
+    @RequestMapping("/queryByCas.do")
+    @ResponseBody
+    public ParseData queryByCas(String cas,Integer page,Integer limit){
+        List<Invoice> list=service.queryByCas(cas);
+        parseData.setCode(0);
+        parseData.setCount(1);
+        parseData.setMsg("");
+        parseData.setData(list);
+        return parseData;
+
+    }
+    @ResponseBody
+    @PostMapping ("/add.do" )
+    public String add(Invoice invoice){
+        try {
+            service.save(invoice);
+        } catch (Exception e) {
+            return Util.defact;
+        }
+        return Util.sueess;
+    }
+
+
 }
