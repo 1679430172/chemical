2c0a99a4c22b2a9bf0ec0865e4b7576fffa1854c
root <myemail@qq.com>
2020-11-05 09:19:22 +0800

    11.5.2

---
+++ src/main/java/com/hy/filter/LoginInterceptor.java
@@ -0,0 +1,14 @@
+package com.hy.filter;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("进入拦截器");
+        return true;
+    }
+}
