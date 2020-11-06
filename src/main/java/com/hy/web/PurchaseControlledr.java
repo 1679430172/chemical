e6ea680c648a51f54728936db394b08fafbf9c8b
xiaojian <1124421223@qq.com>
2020-11-06 08:56:09 +0800

    11.6采购提交

--- src/main/java/com/hy/web/PurchaseControlledr.java
+++ src/main/java/com/hy/web/PurchaseControlledr.java
@@ -1,12 +1,14 @@
 package com.hy.web;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.hy.bean.Order;
 import com.hy.bean.Purchase;
 import com.hy.service.PurchaseService;
 import com.hy.util.ParseData;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,4 +26,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
             return new ParseData(0, "", Integer.parseInt(Long.toString(iPage.getTotal())), iPage.getRecords());
 
         }
+
+        @PostMapping("/add.do")
+        @ResponseBody
+        public Integer add(Order order) throws Exception {
+            System.out.println(order);
+            return 1;
+        }
+
     }
