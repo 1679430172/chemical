fd301b15bff860235e64e3ce4b6b378b9db232de
xiaojian <1124421223@qq.com>
2020-11-19 17:03:46 +0800

    1

--- src/main/java/com/hy/mapper/PurchaseMapper.java
+++ src/main/java/com/hy/mapper/PurchaseMapper.java
@@ -16,7 +16,7 @@ public interface PurchaseMapper extends BaseMapper<Purchase> {
     @Select("select * from purchase")
     public IPage<Purchase> Purchase(Page page);
 
-    @Select("select * from purchase where =#{userId}")
+    @Select("select * from purchase where userId=#{userId}")
     public IPage<Purchase> supplier(@Param("userId") Integer userId,Page page);
 
     @Update("update purchase set ann = 2 where cid=#{cid}")
